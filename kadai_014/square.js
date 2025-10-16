7c2c2be9e8c87a943f7f385d1733a72478e1a831
MT1024-hab <chunwansui@gmail.com>
2025-10-16 16:06:55 +0900

    kadai_014のファイルを提出

---
+++ kadai_014/square.js
@@ -0,0 +1,12 @@
+
+function square_number(number) {
+  // 引数として受け取った数値を二乗します
+  let result = number ** 2;
+  return result;
+}
+// 例として引数に10を渡して関数を実行します
+console.log(square_number(10));
+
+// 計算結果をコンソールに出力します
+console
+
